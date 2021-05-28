a16ae3759eebf9343629d07f2f8483d6b23bfd38
kongyl <kongyl@126.com>
2021-05-28 09:49:21 +0800

    update geoserver 2.19.1

--- postgis/docker.sh
+++
@@ -1,5 +0,0 @@
-# build
-docker build -t kongyl/postgis:13.2-3 .
-# run
-docker run --name postgres --restart=always -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d kongyl/postgis:13.2-3
-docker run --name postgres --restart=always -e POSTGRES_PASSWORD=postgres --network=postgres --ip=10.2.0.2 -d kongyl/postgis:13.2-3
