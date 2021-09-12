d04b40a1f706841a981822ed07185a4a5fd28904
kongyl <kongyl@126.com>
2021-09-12 14:10:48 +0800

    gdal-java

--- cmd/postgres.sh
+++ cmd/postgres.sh
@@ -1,6 +1,5 @@
 # postgres
-docker run --name postgis -e POSTGRES_PASSWORD=postgres --restart=always --network=postgres --ip=10.2.0.2 -d postgis/postgis
-docker run --name postgis -e POSTGRES_PASSWORD=postgres --restart=always -p 5432:5432 -d postgis/postgis
+docker run -d --name postgis -e POSTGRES_PASSWORD=postgres --restart always -p 5432:5432 -v /home/kongyl/postgis/data:/var/lib/postgresql/data postgis/postgis
 
 # pgadmin
 docker run -d --name pgadmin --restart always -p 10080:80 -e 'PGADMIN_DEFAULT_EMAIL=kongyl@126.com' -e 'PGADMIN_DEFAULT_PASSWORD=tuxiangshi' dpage/pgadmin4:5.3
