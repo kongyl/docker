71c0bc601ad502452577c7f3ef238372cc24f205
kongyl <kongyl@126.com>
2022-05-27 20:07:28 +0800

    update geoserver to version 2.21.0

--- geoserver/docker.sh
+++ geoserver/docker.sh
@@ -1,4 +1,4 @@
 # build
-docker build -t kongyl/geoserver:2.19.2 .
+docker build -t kongyl/geoserver:2.21.0 .
 # run
-docker run -d --name geoserver --restart always -p 8090:8080 -v /home/kongyl/Programs/GeoServer:/geoserver kongyl/geoserver:2.19.2
+docker run -d --name geoserver --restart always -p 8090:8080 -v /home/kongyl/Programs/GeoServer:/geoserver kongyl/geoserver:2.21.0
