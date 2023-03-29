f7327d097d06171888d7b04c789ac804d2ffc38a
kongyl <kongyl@126.com>
2023-03-29 16:32:01 +0800

    add pgadmin port

--- cmd/pgadmin.sh
+++ cmd/pgadmin.sh
@@ -1,2 +1,2 @@
 # pgadmin
-docker run -d --name pgadmin -p 20080:80 -e 'PGADMIN_DEFAULT_EMAIL=kongyl@126.com' -e 'PGADMIN_DEFAULT_PASSWORD=tuxiangshi' dpage/pgadmin4:6.15
+docker run -d --name pgadmin -p 20080:80 -e 'PGADMIN_DEFAULT_EMAIL=kongyl@126.com' -e 'PGADMIN_DEFAULT_PASSWORD=tuxiangshi' -e 'PGADMIN_LISTEN_PORT=20080' dpage/pgadmin4:6.21
