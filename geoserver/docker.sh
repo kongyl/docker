efe620c83886b57752441c932a60ea77e0c35403
kongyl <kongyl@126.com>
2023-04-13 10:30:39 +0800

    add java debian

--- geoserver/docker.sh
+++ geoserver/docker.sh
@@ -1,4 +1,4 @@
 # build
-docker build -t kongyl/geoserver:2.21.0 .
+docker build -t kongyl/geoserver:2.23.0 .
 # run
-docker run -d --name geoserver --restart always -p 8090:8080 -v /home/kongyl/Programs/GeoServer:/geoserver kongyl/geoserver:2.21.0
+docker run -d --name geoserver -p 8090:8080 -v /home/kongyl/Test/geoserver:/geoserver docker.io/kongyl/geoserver:2.23.0
