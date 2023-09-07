257b38da1061f6eeaf57511cfd2effc671fb476c
kongyl <kongyl@126.com>
2023-09-07 21:06:59 +0800

    add postgis for podman

--- cmd/postgres.sh
+++ cmd/postgres.sh
@@ -1,2 +1,4 @@
 # postgis
-docker run -d --name postgis -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v /home/kongyl/Test/postgis/data:/var/lib/postgresql/data postgis/postgis:15-3.3
+docker run -d --name postgis -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v /home/kongyl/Test/postgis/data:/var/lib/postgresql/data postgis/postgis:15-3.4
+
+sudo podman run -d --name postgis -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v /home/kongyl/Test/postgis/data:/var/lib/postgresql/data docker.io/postgis/postgis:15-3.4 
