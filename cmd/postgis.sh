742eb450edc305e12acc7a7a82bc44d873d838ac
kongyl <kongyl@126.com>
2024-08-20 09:04:32 +0800

    update cmd of postgis

--- cmd/postgis.sh
+++ cmd/postgis.sh
@@ -1,3 +1,4 @@
 # postgis
-docker run -d --name postgis -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v /root/Test/postgis/data:/var/lib/postgresql/data postgis/postgis:16-3.4
+# init
+docker run -d --name postgis -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v postgis:/var/lib/postgresql/data postgis/postgis:16-3.4
 
